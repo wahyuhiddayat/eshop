689c6450e9bddf5cb6a4f7bcd5eea5edbf51af3a
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-02-03 14:31:09 +0700

    fix some issues

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -17,6 +18,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productRepository.create(product);
         return product;
     }
