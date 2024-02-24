252c19ad9996fe23031872869e101f29959b78c7
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-02-24 10:38:08 +0700

    remove public modifier

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/CarService.java
@@ -2,9 +2,9 @@ package id.ac.ui.cs.advprog.eshop.service;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import java.util.List;
 public interface CarService {
-    public Car create(Car car);
-    public List<Car> findAll();
+    Car create(Car car);
+    List<Car> findAll();
     Car findById(String carId);
-    public void update(String carId, Car car);
-    public void deleteCarById(String carId);
+    void update(String carId, Car car);
+    void deleteCarById(String carId);
 }
