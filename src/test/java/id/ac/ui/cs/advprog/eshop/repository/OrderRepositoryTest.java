ee75319afa59c1bf51f93d7ed138af08ab485325
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-03-05 20:07:42 +0700

    fix typo

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -29,9 +29,9 @@ class OrderRepositoryTest {
         orders = new ArrayList<>();
         Order order1 = new Order("13652556-012a-4c07-b546-54eb1396d79b", products, 1708560000L, "Safira Sudrajat");
         orders.add(order1);
-        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb078", products, 1708560000L, "Safira Sudrajat");
+        Order order2 = new Order("7f9e15bb-4b15-42f4-aebc-c3af385fb078", products, 1708570000L, "Safira Sudrajat");
         orders.add(order2);
-        Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbf1e", products, 1708560000L, "Bambang Sudrajat");
+        Order order3 = new Order("e334ef40-9eff-4da8-9487-8ee697ecbf1e", products, 1708570000L, "Bambang Sudrajat");
         orders.add(order3);
     }
 
