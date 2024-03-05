1db71570698f20d6d6752249a2f0277433da4b21
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-03-05 21:10:15 +0700

    [GREEN] Implement OrderRepository class

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/OrderService.java
@@ -1,5 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.service;
 
+import id.ac.ui.cs.advprog.eshop.model.Order;
+
+import java.util.List;
+
 public interface OrderService {
     public Order createOrder(Order order);
     public Order updateStatus(String orderId, String status);
