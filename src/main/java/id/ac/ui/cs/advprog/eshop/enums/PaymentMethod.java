d8601a3e5d0a47c3003196143c2a7a044ae7d17e
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-03-06 17:02:39 +0700

    [REFACTOR] Implement PaymentMethod enum

--- src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/enums/PaymentMethod.java
@@ -3,19 +3,19 @@ package id.ac.ui.cs.advprog.eshop.enums;
 import lombok.Getter;
 
 @Getter
-public enum PaymentStatus {
+public enum PaymentMethod {
     VOUCHER("VOUCHER_CODE"),
     BANK("BANK_TRANSFER");
 
     private final String value;
 
-    private PaymentStatus(String value) {
+    private PaymentMethod(String value) {
         this.value = value;
     }
 
     public static boolean contains(String param) {
-        for (PaymentStatus paymentStatus : PaymentStatus.values()) {
-            if (paymentStatus.getValue().equals(param)) {
+        for (PaymentMethod paymentMethod : PaymentMethod.values()) {
+            if (paymentMethod.getValue().equals(param)) {
                 return true;
             }
         }
