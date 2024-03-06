d9f9516f86834a0f8fe46a8bcc25b822cb6b162f
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-03-06 19:35:32 +0700

    [REFACTOR] Fix save method

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -10,11 +10,32 @@ public class PaymentRepository {
     private List<Payment> payments = new ArrayList<>();
 
     public Payment save(Payment payment) {
+        int index = -1;
+        for (int i = 0; i < payments.size(); i++) {
+            if (payments.get(i).getId().equals(payment.getId())) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) {
+            payments.set(index, payment);
+        } else {
+            payments.add(payment);
+        }
+        return payment;
     }
 
     public Payment findById(String id) {
+        for (Payment p : payments) {
+            if (p.getId().equals(id)) {
+                return p;
+            }
+        }
+        return null;
     }
 
     public List<Payment> getAllPayments() {
+        return payments;
     }
 }
